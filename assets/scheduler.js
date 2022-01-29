eb311d980a868b517cc2c45094de89747010ef52
Colin Mosi Cargile <cargile10@gmail.com>
2022-01-29 18:58:31 -0500

    I got the current time running concurrently on the page in the header

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -1,39 +1,39 @@
 //make sure everything is linked together through console.log
 //create a table that will hold the planner itself through bootstrap 
 //
-var saveButton = document.getElementById("button-addon2");//this should be the 1st save button on the page 
-var saveInput = document.querySelector("#placeholder");
-var saveUserInput = [];
-var key = ["9am"];
-var value = ["hello there"];
+// var saveButton = document.getElementById("button-addon2");//this should be the 1st save button on the page 
+// var saveInput = document.querySelector("#placeholder");
+// var saveUserInput = [];
+// var key = ["9am"];
+// var value = ["hello there"];
 
-input.form-control == [];
 
 // localStorage.setItem("key", input.form-control);
 
+// button-addon2.addEventListener("click", function(event) {
+//     event.preventDefault();
 
-
-
-
-
-
-
-button-addon2.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    // set new submission to local storage 
-    localStorage.setItem("key", JSON.stringify(input.form-control));
+//     // set new submission to local storage 
+//     localStorage.setItem("key", JSON.stringify(saveInput));
       
+// });
+
+// var input = document.getElementById('saveServer').value;
+// localStorage.setItem('server', input);
+console.log("Hello there");
+
+function displayTime() {
+    var time2 = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $('#clock2').html(time2);
+    setTimeout(displayTime, 1000);
+}
+
+$(document).ready(function() {
+    displayTime();
 });
 
 
 
-
-
-
-// var input = document.getElementById('saveServer').value;
-// localStorage.setItem('server', input);
-// console.log("Hello there");
 
 // document.getElementById("button-addon2").addEventListener("click", myFunction);
 
