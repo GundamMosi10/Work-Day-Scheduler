7a931040fc7f9d0c63dd0026354023f9ed777204
Colin Mosi Cargile <cargile10@gmail.com>
2022-01-29 18:21:41 -0500

    added more thoughts and trying functions to see if it works

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -1,5 +1,42 @@
 //make sure everything is linked together through console.log
 //create a table that will hold the planner itself through bootstrap 
 //
-console.log("Hello there");
+var saveButton = document.getElementById("button-addon2");//this should be the 1st save button on the page 
+var saveInput = document.querySelector("#placeholder");
+var saveUserInput = [];
+var key = ["9am"];
+var value = ["hello there"];
 
+input.form-control == [];
+
+// localStorage.setItem("key", input.form-control);
+
+
+
+
+
+
+
+
+button-addon2.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // set new submission to local storage 
+    localStorage.setItem("key", JSON.stringify(input.form-control));
+      
+});
+
+
+
+
+
+
+// var input = document.getElementById('saveServer').value;
+// localStorage.setItem('server', input);
+// console.log("Hello there");
+
+// document.getElementById("button-addon2").addEventListener("click", myFunction);
+
+// function myFunction() {
+//     document.getElementById("button-addon2").innerHTML = "YOU CLICKED SAVE!";//when clicking on the button the status changes in the button.
+// }
