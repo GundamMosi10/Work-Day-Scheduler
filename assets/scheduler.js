fda9f8320a842370284405d45bd21d49a5453171
Colin Mosi Cargile <cargile10@gmail.com>
2022-01-29 23:18:38 -0500

    added the other button functions from the first button to the last one for thr 9th hour of the work day planner. now Im going to add more to the readme and submit

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -11,6 +11,7 @@ function displayTime() { //this displays the time below the header in the html
 
 $(document).ready(function() {
     displayTime();
+
 });
 
 var militaryTime = parseInt(moment().format("H"));//changing the time to be read as military time and not in the 12 hour format
@@ -32,20 +33,96 @@ $("textarea").each(function() {//this function will help with showing the past, 
     }
 })
 
-
+//For the first hour
 $("button").on("click", function(){
     var hour1 = parseInt($(this).parent().attr("data"));
-    var messtime9 = $("#first-hour").val();
+    var userHour1 = $("#first-hour").val();
 
-    localStorage.setItem("time9", messtime9)
+    localStorage.setItem("hour1", userHour1)
 })
 
-$("#nine-am").append(localStorage.getItem("hour1, messtime10"));
+$("#first-hour").append(localStorage.getItem("hour1"));//allows me to get the info from the local storage to print in the 
+//input field even if we reload the page. 
 
+//This is for the 2nd hour
+$("button").on("click", function(){
+    var hour2 = parseInt($(this).parent().attr("data"));
+    var userHour2 = $("#second-hour").val();
 
+    localStorage.setItem("hour2", userHour2)
+})
 
+$("#second-hour").append(localStorage.getItem("hour2"));
 
+//For the third hour
+$("button").on("click", function(){
+    var hour3 = parseInt($(this).parent().attr("data"));
+    var userHour3 = $("#third-hour").val();
 
+    localStorage.setItem("hour3", userHour3)
+})
+
+$("#third-hour").append(localStorage.getItem("hour3"));
+
+//For the fourth hour
+$("button").on("click", function(){
+    var hour4 = parseInt($(this).parent().attr("data"));
+    var userHour4 = $("#fourth-hour").val();
+
+    localStorage.setItem("hour4", userHour4)
+})
+
+$("#fourth-hour").append(localStorage.getItem("hour4"));
+
+//For the fifth hour
+$("button").on("click", function(){
+    var hour5 = parseInt($(this).parent().attr("data"));
+    var userHour5 = $("#fifth-hour").val();
+
+    localStorage.setItem("hour5", userHour5)
+})
+
+$("#fifth-hour").append(localStorage.getItem("hour5"));
+
+//For the sixth hour
+$("button").on("click", function(){
+    var hour6 = parseInt($(this).parent().attr("data"));
+    var userHour6 = $("#sixth-hour").val();
+
+    localStorage.setItem("hour6", userHour6)
+})
+
+$("#sixth-hour").append(localStorage.getItem("hour6"));
+
+//For the seventh hour
+$("button").on("click", function(){
+    var hour7 = parseInt($(this).parent().attr("data"));
+    var userHour7 = $("#seventh-hour").val();
+
+    localStorage.setItem("hour7", userHour7)
+})
+
+$("#seventh-hour").append(localStorage.getItem("hour7"));
+
+//For the eigth hour
+$("button").on("click", function(){
+    var hour8 = parseInt($(this).parent().attr("data"));
+    var userHour8 = $("#eigth-hour").val();
+
+    localStorage.setItem("hour8", userHour8)
+})
+
+$("#eigth-hour").append(localStorage.getItem("hour8"));
+
+//For the ninth hour
+$("button").on("click", function(){
+    var hour9 = parseInt($(this).parent().attr("data"));
+    var userHour9 = $("#ninth-hour").val();
+
+    localStorage.setItem("hour9", userHour9)
+})
+
+$("#ninth-hour").append(localStorage.getItem("hour9"));
 
 
 
